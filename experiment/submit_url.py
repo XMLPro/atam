75e098a62cd663ae16b7f7d34474ec559460a936
ctare <sankaku900622@yahoo.co.jp>
2018-08-16 15:20:48 +0900

    cookie from string

--- experiment/submit_url.py
+++ experiment/submit_url.py
@@ -1,23 +1,27 @@
 import requests
 import bs4
+from http.cookies import SimpleCookie
 
 #%%
-cookie = {
-}
+cookie_raw = ""
+simple_cookie = SimpleCookie()
+simple_cookie.load(cookie_raw)
+cookie = {k : v.value for k, v in simple_cookie.items()}
 
 csrf_token = None
-def get_csrf_token(force=False):
+base_url = "https://beta.atcoder.jp/contests/"
+def get_csrf_token(prob, force=False):
     global csrf_token
     if force or csrf_token is None:
-        response = requests.get(headers["referer"], cookies=cookie)
+        response = requests.get(base_url + prob, cookies=cookie)
         soup = bs4.BeautifulSoup(response.text, "lxml")
 
         csrf_token = soup.select_one("input[name=csrf_token]")["value"]
     return csrf_token
 
 def submit(prob, prob_number, prob_hard, lang, source_code):
-    csrf_token = get_csrf_token()
-    url = "https://beta.atcoder.jp/contests/{}{}/submit".format(prob, prob_number)
+    csrf_token = get_csrf_token(prob)
+    url = base_url + "{}{}/submit".format(prob, prob_number)
     data = {
         "data.TaskScreenName": "{}{}_{}".format(prob, prob_number, prob_hard),
         "data.LanguageId": lang,
