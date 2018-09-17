e5730529d6f2f51a6b47966c406dee38594efe7d
non258 <c0117037bf@edu.teu.ac.jp>
2018-09-17 11:44:17 +0900

    デバッグ文の削除

--- src/atam.js
+++ src/atam.js
@@ -6,14 +6,3 @@ let command = opt.args;
 let filename = command[0];
 let number = command[1];
 let prob = command[2];
-
-if (command.length > 0) {
-  if (command.length == 3) {
-    console.log('filename:',filename);
-    console.log('number:',number);
-    console.log('prob:',prob);
-  } else {
-    console.log('error');
-    console.log('ex: atam <filename> <number> <prob>');
-  }
-}
