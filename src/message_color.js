0ab6b8ca6bbd4de1a60338ac78bee53623758dde
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-10-22 15:43:03 +0900

    文字の色の定義

---
+++ src/message_color.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk')
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+const success = chalk.keyword('green');
+const info = chalk.keyword('blue');
+
+module.exports = {
+  error,
+  warning,
+  success,
+  info
+}
