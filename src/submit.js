4814e66296166d64d45cf6ca2b21290bdb86adc5
non258 <c0117037bf@edu.teu.ac.jp>
2018-10-25 23:54:27 +0900

    submit.js キャメルケース fix

--- src/submit.js
+++ src/submit.js
@@ -3,16 +3,16 @@ const queryString = require('query-string');
 const fs = require('fs');
 const login = require('./login');
 
-const base_url = 'https://beta.atcoder.jp/contests/';
-const cookie_path = './cookie_login.json';
-const submit = async (logined_page, prob, prob_number, task, lang, source_code) => {
-  const submit_url = `${base_url}${prob}${prob_number}/submit`;
-  const page = logined_page;
+const baseUrl = 'https://beta.atcoder.jp/contests/';
+const cookiePath = './cookie_login.json';
+const submit = async (loginedPage, prob, probNumber, task, lang, sourceCode) => {
+  const submitUrl = `${baseUrl}${prob}${probNumber}/submit`;
+  const page = loginedPage;
 
   const navigationPromise = page.waitForNavigation({
     timeout: 60000, waitUntil: 'domcontentloaded',
   });
-  await page.goto(submit_url);
+  await page.goto(submitUrl);
   await navigationPromise;
 
   await page.screenshot({ path: 'submit_result1.png' }); // debug!!!!!!!!
@@ -20,7 +20,7 @@ const submit = async (logined_page, prob, prob_number, task, lang, source_code) 
   await page.select('select[name="data.TaskScreenName"]', task);
   await page.select('select[name="data.LanguageId"]', lang);
   await page.click('button.btn-toggle-editor');
-  await page.type('textarea[name="sourceCode"]', source_code);
+  await page.type('textarea[name="sourceCode"]', sourceCode);
   page.click('#submit');
   await page.waitForNavigation({ timeout: 60000, waitUntil: 'domcontentloaded' });
 
