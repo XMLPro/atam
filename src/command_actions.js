6e65dcc6d17e0a8dadce1032f256396d8831a37a
ctare <sankaku900622@yahoo.co.jp>
2019-05-08 20:05:56 +0900

    debug用の固定値を修正

--- src/command_actions.js
+++ src/command_actions.js
@@ -10,10 +10,8 @@ async function submit(filename, prob, number) {
   const [page, browser] = await loginMod.loginByCookie();
 
   const sourceCode = gets.getSource(filename);
-  // const lang = await gets.getLangId(page, prob, number);
-  // const task = await gets.getProblemId(page, prob, number);
-  const lang = '3023';
-  const task = 'abc001_1';
+  const lang = await gets.getLangId(page, prob, number);
+  const task = await gets.getProblemId(page, prob, number);
   await submitMod.submit(page, prob, number, task, lang, sourceCode);
   browser.close();
 }
